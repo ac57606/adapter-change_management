f8ca5c8dfd9d336d47ad02bc654defa05842593e
ac57606 <manojkumar.singh@centurylink.com>
2020-09-22 16:03:34 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -146,7 +146,7 @@ class ServiceNowAdapter extends EventEmitter {
    *   handles the response.
    */
   getRecord(callback) {
-      connector.get();
+    connector.get();
     /**
      * Write the body for this function.
      * The function is a wrapper for this.connector's get() method.
@@ -165,7 +165,7 @@ class ServiceNowAdapter extends EventEmitter {
    *   handles the response.
    */
   postRecord(callback) {
-      connector.post();
+     connector.post();
     /**
      * Write the body for this function.
      * The function is a wrapper for this.connector's post() method.
